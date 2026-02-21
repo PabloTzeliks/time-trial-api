925e999e5ea70a2f22b8396bd10c08cac99d2811
PabloTzeliks <arq.pabloo@gmail.com>
2026-02-20 21:11:36 -0300

    initial commit

---
+++ src/main/java/com/centroweg/iot/time_trial_api/TimeTrialApiApplication.java
@@ -0,0 +1,13 @@
+package com.centroweg.iot.time_trial_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimeTrialApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimeTrialApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/centroweg/iot/time_trial_api/TimeTrialApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.centroweg.iot.time_trial_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimeTrialApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
