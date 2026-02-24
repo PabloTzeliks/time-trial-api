c68c2b1071f2de75e207c002bfb8c015f7cc42bd
jonathan_uber <jonathanuber121207@gmail.com>
2026-02-23 21:28:16 -0300

    feat: create config classes to start mqtt comunication

---
+++ src/main/java/com/centroweg/iot/time_trial_api/config/MqttProperties.java
@@ -0,0 +1,15 @@
+package com.centroweg.iot.time_trial_api.config;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Data
+@Configuration
+@ConfigurationProperties(prefix = "mqtt")
+public class MqttProperties {
+
+    private String broker;
+    private String clientId;
+    private String topic;
+}
