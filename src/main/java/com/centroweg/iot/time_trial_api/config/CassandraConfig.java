9294dd965d2f8907196aff84cd58a8baa3b65b8a
PabloTzeliks <arq.pabloo@gmail.com>
2026-03-05 16:05:57 -0300

    chore: add enviorment vars about cassandra container

--- src/main/java/com/centroweg/iot/time_trial_api/config/CassandraConfig.java
+++ src/main/java/com/centroweg/iot/time_trial_api/config/CassandraConfig.java
@@ -1,24 +1,42 @@
 package com.centroweg.iot.time_trial_api.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.cassandra.config.AbstractCassandraConfiguration;
 
 @Configuration
 public class CassandraConfig extends AbstractCassandraConfiguration {
 
+    @Value("${spring.cassandra.keyspace-name:time_trial}")
+    private String keyspaceName;
+
+    @Value("${spring.cassandra.contact-points:localhost}")
+    private String contactPoints;
+
+    @Value("${spring.cassandra.port:9042}")
+    private int port;
+
+    @Value("${spring.cassandra.local-datacenter:datacenter1}")
+    private String localDatacenter;
+
     @Override
     protected String getKeyspaceName() {
-        return "time_trial";
+        return keyspaceName;
     }
 
     @Override
     protected String getContactPoints() {
-        return "localhost";
+        return contactPoints;
     }
 
     @Override
     protected int getPort() {
-        return 9042;
+        return port;
+    }
+
+    @Override
+    protected String getLocalDataCenter() {
+        return localDatacenter;
     }
 
     @Override
