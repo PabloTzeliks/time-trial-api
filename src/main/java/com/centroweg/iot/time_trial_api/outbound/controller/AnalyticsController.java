8ff88c453fe2592cc56755fb762639d62bb7e92d
PabloTzeliks <arq.pabloo@gmail.com>
2026-03-10 19:29:35 -0300

    feat: implement 'Analytics' controller

--- src/main/java/com/centroweg/iot/time_trial_api/outbound/controller/AnalyticsController.java
+++ src/main/java/com/centroweg/iot/time_trial_api/outbound/controller/AnalyticsController.java
@@ -1,11 +1,29 @@
 package com.centroweg.iot.time_trial_api.outbound.controller;
 
+import com.centroweg.iot.time_trial_api.core.service.AnalyticsService;
+import com.centroweg.iot.time_trial_api.outbound.dto.HistoricoCarroResponseDTO;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/analytics")
 @CrossOrigin(origins = "*")
 public class AnalyticsController {
+
+    private final AnalyticsService analyticsService;
+
+    public AnalyticsController(AnalyticsService analyticsService) {
+        this.analyticsService = analyticsService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<HistoricoCarroResponseDTO>> getAnalytics() {
+
+        return ResponseEntity.ok(analyticsService.listaHistoricoCarro());
+    }
 }
