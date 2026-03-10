257023a443cd8738b3930faa8ecd25ab8ba4b9a2
PabloTzeliks <arq.pabloo@gmail.com>
2026-03-10 19:55:53 -0300

    feat: implement full GlobalExceptionHandler with Errors DTO

--- src/main/java/com/centroweg/iot/time_trial_api/outbound/dto/error/RespostaErro.java
+++ src/main/java/com/centroweg/iot/time_trial_api/outbound/dto/error/RespostaErro.java
@@ -2,12 +2,15 @@ package com.centroweg.iot.time_trial_api.outbound.dto.error;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 
+import java.time.LocalDateTime;
+
 @JsonInclude(JsonInclude.Include.NON_NULL)
-public record ErrorResponse(
+public record RespostaErro(
 
         String type,
         String title,
         int status,
         String detail,
-        String instance
+        String instance,
+        LocalDateTime time
 ) { }
