16f3c46d8c0afcb801e663a97e73105b2e5a9dc0
PabloTzeliks <arq.pabloo@gmail.com>
2026-03-10 19:46:43 -0300

    feat: implement rvalidation for when resource not found

---
+++ src/main/java/com/centroweg/iot/time_trial_api/core/domain/exception/RecursoNaoEncontradoException.java
@@ -0,0 +1,7 @@
+package com.centroweg.iot.time_trial_api.core.domain.exception;
+
+public class RecursoNaoEncontradoException extends RuntimeException {
+    public RecursoNaoEncontradoException(String message) {
+        super(message);
+    }
+}
