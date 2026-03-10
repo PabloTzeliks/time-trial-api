463652cccaf2d6893aedf4f72cc4ff24c6e0b908
PabloTzeliks <arq.pabloo@gmail.com>
2026-03-10 19:21:16 -0300

    feat: implement initial Service for Analytics

---
+++ src/main/java/com/centroweg/iot/time_trial_api/outbound/controller/AnalyticsController.java
@@ -0,0 +1,11 @@
+package com.centroweg.iot.time_trial_api.outbound.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/analytics")
+@CrossOrigin(origins = "*")
+public class AnalyticsController {
+}
