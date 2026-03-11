289dd4433b700f67a95c907224acfd3ab6be1dc6
PabloTzeliks <arq.pabloo@gmail.com>
2026-03-11 09:58:24 -0300

    fix: implement Cassandra Quorum directly on CassandraConfig class

--- src/main/java/com/centroweg/iot/time_trial_api/config/CassandraConfig.java
+++ src/main/java/com/centroweg/iot/time_trial_api/config/CassandraConfig.java
@@ -1,8 +1,10 @@
 package com.centroweg.iot.time_trial_api.config;
 
+import com.datastax.oss.driver.api.core.config.DefaultDriverOption;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.cassandra.config.AbstractCassandraConfiguration;
+import org.springframework.data.cassandra.config.DriverConfigLoaderBuilderConfigurer;
 
 @Configuration
 public class CassandraConfig extends AbstractCassandraConfiguration {
@@ -16,8 +18,11 @@ public class CassandraConfig extends AbstractCassandraConfiguration {
     @Value("${spring.cassandra.port:9042}")
     private int port;
 
-    @Value("${spring.cassandra.local-datacenter:datacenter1}")
+    @Value("${spring.cassandra.local-datacenter:dc1}") // <- Já deixei o default seguro como dc1
     private String localDatacenter;
+
+    @Value("${spring.cassandra.request.consistency:QUORUM}") // <- Lendo a consistência
+    private String consistencyLevel;
 
     @Override
     protected String getKeyspaceName() {
@@ -42,5 +47,10 @@ public class CassandraConfig extends AbstractCassandraConfiguration {
     @Override
     public String[] getEntityBasePackages() {
         return new String[]{"com.centroweg.iot.time_trial_api.core.domain"};
+    }
+
+    @Override
+    protected DriverConfigLoaderBuilderConfigurer getDriverConfigLoaderBuilderConfigurer() {
+        return builder -> builder.withString(DefaultDriverOption.REQUEST_CONSISTENCY, consistencyLevel);
     }
 }
