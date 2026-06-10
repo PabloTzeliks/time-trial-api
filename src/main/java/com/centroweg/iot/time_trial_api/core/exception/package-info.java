43b50fb4ebb69bfade24f9b07696cd4cd13a07a2
PabloTzeliks <arq.pabloo@gmail.com>
2026-06-10 20:49:12 -0300

    feat: declare NamedInterface for core module public sub-packages

---
+++ src/main/java/com/centroweg/iot/time_trial_api/core/exception/package-info.java
@@ -0,0 +1,4 @@
+@NamedInterface("exceptions")
+package com.centroweg.iot.time_trial_api.core.exception;
+
+import org.springframework.modulith.NamedInterface;
---
+++ src/main/java/com/centroweg/iot/time_trial_api/core/service/package-info.java
@@ -0,0 +1,4 @@
+@NamedInterface("services")
+package com.centroweg.iot.time_trial_api.core.service;
+
+import org.springframework.modulith.NamedInterface;
